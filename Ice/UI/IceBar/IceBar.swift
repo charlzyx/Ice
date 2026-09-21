895117b41461d447b17cf663d5a727cad2b42001
yangxiaochao <yangxiaochao@didiglobal.com>
2026-09-22 00:55:35 +0800

    修复 WindowInfo 可选值嵌套导致的编译错误

--- Ice/UI/IceBar/IceBar.swift
+++ Ice/UI/IceBar/IceBar.swift
@@ -67,7 +67,7 @@ final class IceBarPanel: NSPanel {
                         // Only continue if the menu bar is automatically hidden, as Ice
                         // can't currently display its menu bar items.
                         appState.menuBarManager.isMenuBarHiddenBySystemUserDefaults,
-                        let info = window.flatMap({ CGWindowID(exactly: $0.windowNumber) }).map({ WindowInfo(windowID: $0) }),
+                        let info = window.flatMap({ CGWindowID(exactly: $0.windowNumber) }).flatMap({ WindowInfo(windowID: $0) }),
                         // Window being offscreen means the menu bar is currently hidden.
                         // Close the bar, as things will start to look weird if we don't.
                         !info.isOnScreen
